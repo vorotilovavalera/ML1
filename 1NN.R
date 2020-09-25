b21a72d74f88e821225c5f631caf205e72f2a1d7
Валерия <71381520+vorotilovavalera@users.noreply.github.com>
2020-09-25 10:33:01 +0300

    Update 1NN.R

--- 1NN.R
+++ 1NN.R
@@ -3,33 +3,33 @@ euclideanDistance <- function(u, v)
 { 
   sqrt(sum((u - v)^2)) 
 } 
-##Ñîðòèðóåì îáúåêòû ñîãëàñíî ðàññòîÿíèÿ äî îáúåêòà z
+##Сортируем объекты согласно расстояния до объекта z
 sortObjectByDist <- function(xl, z, metricFunction = euclideanDistance) 
 { 
   l <- dim(xl)[1] 
   n <- dim(xl)[2] - 1 
-  ##Ñîçäàåì ìàòðèöó ðàññòîÿíèé
+  ##Создаем матрицу расстояний
   distances <- matrix(NA, l, 2)
   
   for (i in 1:l) 
   { 
     distances[i] <- c(metricFunction(xl[i, 1:n], z)) 
   } 
-  #Ñîðòèðóåì
+  #Сортируем
   orderedXl <- xl[order(distances), ] 
   return (orderedXl) 
 } 
-#Ïðèìåíÿåì ìåòîä 1NN
+#Применяем метод 1NN
 NN<-function(xl, z) 
 { 
-  ##Ñîðòèðóåì âûáîðêó ñîãëàñíî êëàññèôèöèðóåìîãî îáúåêòà
+  ##Сортируем выборку согласно классифицируемого объекта
   orderedXl<- sortObjectByDist(xl, z) 
   n<-dim(orderedXl)[2] 
   
   classes <- orderedXl[1, n] 
   return (classes) 
 } 
-##Ðèñóåì âûáîðêó
+##Рисуем выборку
 colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue") 
 X<-sample(c(1:150), 25, replace=TRUE) 
 xl <- iris[X, 3:5] 
