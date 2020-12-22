1e387d9952a5fa129f880639144e677f06b175ce
Валерия <71381520+vorotilovavalera@users.noreply.github.com>
2020-12-22 14:09:25 +0300

    Update все.R

--- linear/все.R
+++ linear/все.R
@@ -1,3 +1,4 @@
+## Нормализация обучающей выборки
 trainingSampleNormalization <- function(xl)
 {
   n <- dim(xl)[2] - 1
@@ -7,160 +8,71 @@ trainingSampleNormalization <- function(xl)
   }
   return (xl)
 }
+## Добавление колонки для из -1 для w0
 
 trainingSamplePrepare <- function(xl)
-{
-  l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1
-  xl <- cbind(xl[, 1:n], seq(from = -1, to = -1, length.out = l), xl[, n + 1])
-}
-
-lossQuad <- function(x)
-{
-  return ((x-1)^2)
-}
-
-## ADALINE
-sg.ADALINE <- function(xl, eta = 1, lambda = 1/6)
-{
-  l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1
-  w <- c(1/2, 1/2, 1/2)
   
-  iterCount <- 0
-  Q <- 0
-  for (i in 1:l)
-  {
-    
-    wx <- sum(w * xl[i, 1:n])
-    margin <- wx * xl[i, n + 1]
-    Q <- Q + lossQuad(margin)
-  }
-  repeat
-  { 
-    margins <- array(dim = l)
-    for (i in 1:l)
-    {
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      margins[i] <- crossprod(w, xi) * yi
-    }
-    errorIndexes <- which(margins <= 0)
-    if (length(errorIndexes) > 0)
-    {
-      
-      i <- sample(errorIndexes, 1)
-      iterCount <- iterCount + 1
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      
-      wx <- sum(w * xi)
-      margin <- wx * yi
-      ex <- lossQuad(margin)
-      eta <- 1 / sqrt(sum(xi * xi))
-      w <- w - eta * (wx - yi) * xi
-      Qprev <- Q
-      Q <- (1 - lambda) * Q + lambda * ex
-    }
-    else
-    {
-      break
-    }
-  }
-  return (w)
-}
-
-
-
-lossPerceptron <- function(x)
 {
-  return (max(-x, 0))
-}
-
-
-sg.Hebb <- function(xl, eta = 0.1, lambda = 1/6)
-{
+  
   l <- dim(xl)[1]
+  
   n <- dim(xl)[2] - 1
-  w <- c(1/2, 1/2, 1/2)
-  iterCount <- 0
-  Q <- 0
-  for (i in 1:l)
-  {
-    
-    wx <- sum(w * xl[i, 1:n])
-    margin <- wx * xl[i, n + 1]
-    Q <- Q + lossPerceptron(margin)
-  }
-  repeat
-  {
-    
-    margins <- array(dim = l)
-    for (i in 1:l)
-    {
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      margins[i] <- crossprod(w, xi) * yi
-    }
-    
-    errorIndexes <- which(margins <= 0)
-    if (length(errorIndexes) > 0)
-    {
-      
-      i <- sample(errorIndexes, 1)
-      iterCount <- iterCount + 1
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      w <- w + eta * yi * xi
-    }
-    else
-      break;
-  }
-  return (w)
+  
+  xl <- cbind(xl[, 1:n], seq(from = -1, to = -1, length.out = l), xl[, n + 1])
+  
 }
 
 
 
-lossLog <- function(x)
+## Логарифмическая функция потерь
+
+lossSigmoid <- function(x)
+  
 {
+  
   return (log2(1 + exp(-x)))
+  
 }
-
+## Сигмоидная функция
 sigmoidFunction <- function(z)
 {
   return (1 / (1 + exp(-z)))
 }
-
-
-sg.Regression <- function(xl, eta = 0.1, lambda = 1/6)
+## Стохастический градиент для логистической регрессии 
+sg.LogRegression <- function(xl)
 {
   l <- dim(xl)[1]
   n <- dim(xl)[2] - 1
   w <- c(1/2, 1/2, 1/2)
   iterCount <- 0
-  
+  lambda <- 1/l
+  ## Инициализация Q
   Q <- 0
   for (i in 1:l)
   {
-    
+    ## Считаем скаляр <w,x>
     wx <- sum(w * xl[i, 1:n])
+    ## Считаем отступ
     margin <- wx * xl[i, n + 1]
-    Q <- Q + lossLog(margin)
+    Q <- Q + lossSigmoid(margin)
   }
+  
   repeat
-  {
     
+  {
+    # Рандомный игдекс ошибки
     i <- sample(1:l, 1)
     iterCount <- iterCount + 1
     xi <- xl[i, 1:n]
     yi <- xl[i, n + 1]
-    
+    ## считаем скаляр <w,xi>
     wx <- sum(w * xi)
+    ## делаем градиентный шаг
     margin <- wx * yi
-    ex <- lossLog(margin)
-    eta <- 0.3
+    ex <- lossSigmoid(margin)
+    eta <- 0.3#1 / iterCount
     w <- w + eta * xi * yi * sigmoidFunction(-wx * yi)
-    
+    ## считаем новое  Q
     Qprev <- Q
     Q <- (1 - lambda) * Q + lambda * ex
     if (abs(Qprev - Q) / abs(max(Qprev, Q)) < 1e-5)
@@ -168,42 +80,32 @@ sg.Regression <- function(xl, eta = 0.1, lambda = 1/6)
   }
   return (w)
 }
+ObjectsCountOfEachClass <- 100
 
-ObjectsCountOfEachClass <- 200
+library(MASS)
+Sigma1 <- matrix(c(10, 0, 0, 10), 2, 2)
+Sigma2 <- matrix(c(4, 1, 1, 2), 2, 2)
+xy1 <- mvrnorm(n=ObjectsCountOfEachClass, c(0, 0), Sigma1)
+xy2 <- mvrnorm(n=ObjectsCountOfEachClass, c(10, -10), Sigma2)
+xl <- rbind(cbind(xy1, -1), cbind(xy2, +1))
+colors <- c(rgb(255/255, 255/255, 0/255), "white", rgb(0/255, 200/255, 0/255))
+## Нормализация
+xlNorm <- trainingSampleNormalization(xl)
+xlNorm <- trainingSamplePrepare(xlNorm)
+## отображение
 
-test <- function(ObjectsCountofEachClass) {
-  library(MASS)
-  sigma <- matrix(c(3,0,0,7),2,2)
-  
-  xy1 <- mvrnorm (ObjectsCountofEachClass,c(1,1),sigma)
-  xy2 <- mvrnorm (ObjectsCountofEachClass,c(9,7),sigma)
-  
-  xl <- rbind(cbind(xy1, 1), cbind(xy2, -1))
-  print(xl);
-  
-  xlNorm <- trainingSampleNormalization(xl)
-  xlNorm <- trainingSamplePrepare(xlNorm)
-  
-  colors <- c("green2", "red")
-  plot(xlNorm[, 1], xlNorm[, 2], pch = 21, asp = 1, main="Все линейные алгоритмы классификации")
-  
-  for (i in 1:dim(xlNorm)[1]) {
-    points(xlNorm[i, 1], xlNorm[i, 2], pch = 21, bg = colors[ifelse((xl[i, 3] < 0), 1, 2)], asp = 1)
-  }
-  w <- sg.ADALINE(xlNorm)
-  
-  abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col = "darkblue")
-  
-  w <- sg.Hebb(xlNorm)
-  
-  abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col = "orange")
-  
-  
-  w <- sg.Regression(xlNorm)
-  
-  abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col ="purple")
-  
-  legend("bottomleft", c("ADALINE", "Персептрон Розенблатта", "Логистическая регрессия"), pch = c(10,10,10), col =c("darkblue", "orange", "purple"))
-}
+plot(xlNorm[, 1], xlNorm[, 2], pch = 21, bg = colors[xl[,3] + 2], asp = 1, xlab = "X", ylab = "Y", main = "Линейные классификаторы")
+w <- sg.LogRegression(xlNorm)
+abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 2, col = "black")
 
-test(60)
+
+## ADALINE 
+w <- sg.ADALINE(xlNorm)  
+abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 2, col = "blue") 
+
+
+## Правило Хебба 
+w <- sg.Hebb(xlNorm)     
+abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 2, col = "red") 
+
+# legend("bottomleft", c("ADALINE", "Правило Хэбба", "Логистическая регрессия"), pch = c(15,15,15), col = c("blue", "red", "black")) 
