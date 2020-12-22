4c152416c884dda6a55e97567483f5c322f3e782
Валерия <71381520+vorotilovavalera@users.noreply.github.com>
2020-12-22 13:47:35 +0300

    Update adaline.R

--- linear/adaline.R
+++ linear/adaline.R
@@ -1,102 +1,91 @@
-lossQuad <- function(x)
+##Нормализация обучающей выборки
+trainingSampleNormalization <- function(xl)  
 {
-  return ((x-1)^2)
-} 
-
-#нормализация начальных данных
-trainingSampleNormalization <- function(xl) {
   n <- dim(xl)[2] - 1
-  for (i in 1:n) {
+   for(i in 1:n)
+  {
     xl[, i] <- (xl[, i] - mean(xl[, i])) / sd(xl[, i])
   }
-  return(xl)
+  return (xl)
 }
-
-# Добавление колонки из -1 для w0
-trainingSamplePrepare <- function(xl) {
+## Добавление колонки для из -1 для w0
+trainingSamplePrepare <- function(xl)
+{
   l <- dim(xl)[1]
   n <- dim(xl)[2] - 1
   xl <- cbind(xl[, 1:n], seq(from = -1, to = -1, length.out = l), xl[, n + 1])
 }
-
-# применяем метод стохастического градиента
-sg <- function(xl, eta = 0.2, lambda = 1/60) {
-  l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1
-  
-  # стандартная инициализация весов w
-  w <- c(1/2, 1/2, 1/2)
-  
-  iterCount <- 0
-  # определяем Q
-  Q <- 0
-  for (i in 1:l) {
-    wx <- sum(w * xl[i, 1:n])
-    margin <- wx * xl[i, n + 1]
-    Q <- Q + lossQuad(margin)
-  }
-  repeat {
-    
-    margins <- array(dim = l)
-    for (i in 1:l)
-    {
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      margins[i] <- crossprod(w, xi) * yi ##находим отступ
-    }
-    
-    errorIndexes <- which(margins <= 0) ##инициализируем объекты, на которых выполняется ошибка
-    
-    if (length(errorIndexes) > 0)
-    {
-      # случайным образом выбираем индекс из объектов ошибки
-      i <- sample(1:l, 1)
-      iterCount <- iterCount + 1
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      
-      wx <- crossprod(w, xi)
-      margin <- wx * yi
-      ex <- lossQuad(margin)
-      w <- w - eta * (wx - yi) * xi ##расчитываем градиентный шаг для итерации
-      
-      Qprev <- Q
-      Q <- (1 - lambda) * Q + lambda * ex ##обновляем Q
-      
-    } else
-    {
-      break
-    }
-  }
-  return(w) 
-}
-
-test <- function(ObjectsCountofEachClass) {
-  library(MASS)
-  sigma <- matrix(c(3,0,0,7),2,2)
-  
-  xy1 <- mvrnorm (ObjectsCountofEachClass,c(1,1),sigma)
-  xy2 <- mvrnorm (ObjectsCountofEachClass,c(9,7),sigma)
-  
-  xl <- rbind(cbind(xy1, 1), cbind(xy2, -1))
-  print(xl);
-  
-  xlNorm <- trainingSampleNormalization(xl)
-  xlNorm <- trainingSamplePrepare(xlNorm)
-  
-  colors <- c("green2", "red")
-  plot(xlNorm[, 1], xlNorm[, 2], pch = 21, asp = 1, main="Линейные алгоритмы классификации.ADALINE.")
-  
-  for (i in 1:dim(xlNorm)[1]) {
-    points(xlNorm[i, 1], xlNorm[i, 2], pch = 21, bg = colors[ifelse((xl[i, 3] < 0), 1, 2)], asp = 1)
-  }
-  w <- sg(xlNorm)
-  print(w)
-  
-  abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col = "blue")
-  
-  
-}
-
-
-test(100)
+## Квадратичная функция потерь 
+lossQuad <- function(x) {     return ((x-1)^2) }
+## Стохастический градиент для ADALINE 
+sg.ADALINE <- function(xl, eta = 1, lambda = 1/6) 
+  {     
+  l <- dim(xl)[1]     
+  n <- dim(xl)[2] - 1        
+  w <- c(1/2, 1/2, 1/2)     
+  iterCount <- 0        
+  ## initialize Q     
+  Q <- 0     
+  for (i in 1:l)     
+    {         
+    ## calculate the scalar product <w,x>        
+    wx <- sum(w * xl[i, 1:n])            
+    ## calculate a margin          
+    margin <- wx * xl[i, n + 1]                  
+    Q <- Q + lossQuad(margin)        
+    }       
+  repeat    
+    {        
+      ## calculate the margins for all objects of the training sample        
+      margins <- array(dim = l)          
+for (i in 1:l)       
+{            
+  xi <- xl[i, 1:n]             
+  yi <- xl[i, n + 1]                         
+  margins[i] <- crossprod(w, xi) * yi          
+}              
+      ## select the error objects         
+      errorIndexes <- which(margins <= 0)             
+      if (length(errorIndexes) > 0)         
+      {            
+        # select the random index from the errors             
+        i <- sample(errorIndexes, 1)            
+        iterCount <- iterCount + 1                          
+        xi <- xl[i, 1:n]            
+        yi <- xl[i, n + 1]                          
+        ## calculate the scalar product <w,xi>            
+        wx <- sum(w * xi)                          
+        ## make a gradient step             
+        margin <- wx * yi                         
+        ## calculate an error             
+        ex <- lossQuad(margin)             
+        eta <- 1 / sqrt(sum(xi * xi))            
+        w <- w - eta * (wx - yi) * xi                            
+        ## Calculate a new Q             
+        Qprev <- Q             
+        Q <- (1 - lambda) * Q + lambda * ex        
+      }         
+      else         
+      {             
+          break         
+      }     
+      }  
+return (w) 
+  } 
+ObjectsCountOfEachClass <- 50
+## Моделируем обучающие данные
+library(MASS)
+Sigma1 <- matrix(c(2, 0, 0, 10), 2, 2)
+Sigma2 <- matrix(c(4, 1, 1, 2), 2, 2)
+xy1 <- mvrnorm(n=ObjectsCountOfEachClass, c(0, 0), Sigma1)
+xy2 <- mvrnorm(n=ObjectsCountOfEachClass, c(10, -10), Sigma2)
+xl <- rbind(cbind(xy1, -1), cbind(xy2, +1))
+colors <- c(rgb(255/255, 255/255, 0/255), "white", rgb(0/255, 200/255, 0/255))
+## нормализация данных
+xlNorm <- trainingSampleNormalization(xl)
+xlNorm <- trainingSamplePrepare(xlNorm)
+## отображение
+plot(xlNorm[, 1], xlNorm[, 2], pch = 21, bg = colors[xl[,3] + 2], asp = 1, xlab = "x1", ylab = "x2", main = "Линейные классификатор")
+## Ada
+w <- sg.ADALINE(xlNorm)  
+abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 2, col = "blue")
