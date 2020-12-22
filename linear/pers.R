1c7f7cccfff87e7cfc85a67b8f24c787eb123705
Валерия <71381520+vorotilovavalera@users.noreply.github.com>
2020-12-22 14:03:26 +0300

    Update pers.R

--- linear/pers.R
+++ linear/pers.R
@@ -1,101 +1,82 @@
-lossPerceptron <- function(x)
+##Нормализация обучающей выборки
+trainingSampleNormalization <- function(xl)  
+{  
+n <- dim(xl)[2] - 1  
+  for(i in 1:n)    
+  {    
+    xl[, i] <- (xl[, i] - mean(xl[, i])) / sd(xl[, i])    
+  }  
+  return (xl)  
+}
+## Добавление колонки для из -1 для w0
+trainingSamplePrepare <- function(xl)  
+{  
+  l <- dim(xl)[1]  
+  n <- dim(xl)[2] - 1  
+  xl <- cbind(xl[, 1:n], seq(from = -1, to = -1, length.out = l), xl[, n + 1])  
+}
+## Функция потерь для правила Хэбба
+lossPerceptron <- function(x) 
+{  
+  return (max(-x, 0))  
+}
+## Стохастический градиент с правилом Хебба
+sg.Hebb <- function(xl, eta = 0.1, lambda = 1/6)  
 {
-  return (max(-x, 0))
-}
-
-trainingSampleNormalization <- function(xl) {
+  l <- dim(xl)[1] 
   n <- dim(xl)[2] - 1
-  for (i in 1:n) {
-    xl[, i] <- (xl[, i] - mean(xl[, i])) / sd(xl[, i])
-  }
-  return(xl)
-}
-
-
-trainingSamplePrepare <- function(xl) {
-  l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1
-  xl <- cbind(xl[, 1:n], seq(from = -1, to = -1, length.out = l), xl[, n + 1])
-}
-
-
-sg <- function(xl, eta = 0.2, lambda = 1/60) {
-  l <- dim(xl)[1]
-  n <- dim(xl)[2] - 1
-  
-
-  w <- c(1/2, 1/2, 1/2)
-  
+  w <- c(1/2, 1/2, 1/2)  
   iterCount <- 0
-
+  ## initialize Q
   Q <- 0
-  for (i in 1:l) {
-    wx <- sum(w * xl[i, 1:n])
-    margin <- wx * xl[i, n + 1]
-    Q <- Q + lossPerceptron(margin)
-  }
-  repeat {
-    
-    margins <- array(dim = l)
-    for (i in 1:l)
-    {
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      margins[i] <- crossprod(w, xi) * yi 
-    }
-    
-    errorIndexes <- which(margins <= 0) 
-    
-    if (length(errorIndexes) > 0)
-    {
-     
-      i <- sample(1:l, 1)
-      iterCount <- iterCount + 1
-      xi <- xl[i, 1:n]
-      yi <- xl[i, n + 1]
-      
-      wx <- crossprod(w, xi)
-      margin <- wx * yi
-      ex <- lossPerceptron(margin)
-      w <- w + eta * yi * xi 
-      
-      Qprev <- Q
-      Q <- (1 - lambda) * Q + lambda * ex 
-      
-    } else
-    {
-      break
-    }
-  }
-  return(w) 
+  for (i in 1:l)
+  {  
+  ## Суммируем скаляры <w,x>    
+    wx <- sum(w * xl[i, 1:n])    
+    ## считаем отступы    
+    margin <- wx * xl[i, n + 1]    
+    Q <- Q + lossPerceptron(margin)    
+  }  
+  repeat    
+  {    
+    ## Поиск ошибочных объектов    
+    margins <- array(dim = l)    
+    for (i in 1:l)      
+    {      
+      xi <- xl[i, 1:n]      
+      yi <- xl[i, n + 1]      
+      margins[i] <- crossprod(w, xi) * yi      
+    }    
+    ## выбрать ошибочные объекты    
+    errorIndexes <- which(margins <= 0)    
+    if (length(errorIndexes) > 0)      
+    {      
+      #  выбрать случайный ошибочный объект      
+      i <- sample(errorIndexes, 1)      
+      iterCount <- iterCount + 1      
+      xi <- xl[i, 1:n]      
+      yi <- xl[i, n + 1]      
+      w <- w + eta * yi * xi      
+    }    
+    else      
+      break;    
+  }  
+  return (w)  
 }
-
-test <- function(ObjectsCountofEachClass) {
-  library(MASS)
-  sigma <- matrix(c(3,0,0,7),2,2)
-  
-  xy1 <- mvrnorm (ObjectsCountofEachClass,c(1,1),sigma)
-  xy2 <- mvrnorm (ObjectsCountofEachClass,c(9,7),sigma)
-  
-  xl <- rbind(cbind(xy1, 1), cbind(xy2, -1))
-  print(xl);
-  
-  xlNorm <- trainingSampleNormalization(xl)
-  xlNorm <- trainingSamplePrepare(xlNorm)
-  
-  colors <- c("green2", "red")
-  plot(xlNorm[, 1], xlNorm[, 2], pch = 21, asp = 1, main="Персептрон Розенблатта")
-  
-  for (i in 1:dim(xlNorm)[1]) {
-    points(xlNorm[i, 1], xlNorm[i, 2], pch = 21, bg = colors[ifelse((xl[i, 3] < 0), 1, 2)], asp = 1)
-  }
-  w <- sg(xlNorm)
-  print(w)
-  
-  abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col = "orange")
-  
-  
-}
-
-
-test(50)
+ObjectsCountOfEachClass <- 50
+## Моделируем обучающие данные
+library(MASS)
+Sigma1 <- matrix(c(2, 0, 0, 10), 2, 2)
+Sigma2 <- matrix(c(4, 1, 1, 2), 2, 2)
+xy1 <- mvrnorm(n=ObjectsCountOfEachClass, c(0, 0), Sigma1)
+xy2 <- mvrnorm(n=ObjectsCountOfEachClass, c(10, -10), Sigma2)
+xl <- rbind(cbind(xy1, -1), cbind(xy2, +1))
+colors <- c(rgb(255/255, 255/255, 0/255), "white", rgb(0/255, 200/255, 0/255))
+## нормализация данных
+xlNorm <- trainingSampleNormalization(xl)
+xlNorm <- trainingSamplePrepare(xlNorm)
+## отображение
+plot(xlNorm[, 1], xlNorm[, 2], pch = 21, bg = colors[xl[,3] + 2], asp = 1, xlab = "x1", ylab = "x2", main = "Персептрон Розенблатта")
+## Правило Хэбба
+w <- sg.Hebb(xlNorm)
+abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col = "green3")
